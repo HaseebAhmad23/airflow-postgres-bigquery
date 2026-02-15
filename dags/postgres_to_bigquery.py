2672d81c73cf9b9015be7c90ef58612c9fb4df96
HaseebAhmad23 <haseeb26@live.com>
2026-02-15 15:18:12 +0100

    extend pipeline to load customer, order, order_positions and products tables

--- dags/postgres_to_bigquery.py
+++ dags/postgres_to_bigquery.py
@@ -5,32 +5,57 @@ import pandas as pd
 import psycopg2
 from google.cloud import bigquery
 
+POSTGRES_HOST = "172.17.0.1"
+POSTGRES_DB = "webshop"
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "postgres"
+
+BQ_PROJECT = "postgresql-store-database"
+BQ_DATASET = "raw_data"
+
+TABLES = [
+    "customer",
+    "products",
+    "order",
+    "order_positions"
+]
+
 def extract_and_load():
     # Connect to Postgres
-    conn = psycopg2.connect(
-        host="172.17.0.1",   # Important!
-        database="webshop",
-        user="postgres",
-        password="postgres"
+    connection = psycopg2.connect(
+        host=POSTGRES_HOST,
+        database=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD
     )
 
-    query = "SELECT * FROM webshop.customer;"
-    df = pd.read_sql(query, conn)
-    conn.close()
-
-    if df.empty:
-        print("No data found.")
-        return
-
-    print("Postgres data found.")
-
     client = bigquery.Client()
-    table_id = "postgresql-store-database.raw_data.customer"
 
-    job = client.load_table_from_dataframe(df, table_id)
-    job.result()
+    for table in TABLES:
+        print(f"Processing table: {table}")
 
-    print(f"Loaded {len(df)} rows to BigQuery.")
+        query = f"SELECT * FROM webshop.{table};"
+        df = pd.read_sql(query, connection)
+
+        if df.empty:
+            print(f"No data found in {table}")
+            continue
+
+        table_id = f"{BQ_PROJECT}.{BQ_DATASET}.{table}"
+
+        job = client.load_table_from_dataframe(
+            df,
+            table_id,
+            job_config=bigquery.LoadJobConfig(
+                write_disposition="WRITE_TRUNCATE"
+            )
+        )
+
+        job.result()
+
+        print(f"Loaded {len(df)} rows into {table}")
+
+    connection.close()
 
 with DAG(
     dag_id="postgres_to_bigquery",
